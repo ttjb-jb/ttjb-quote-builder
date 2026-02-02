b73428c40a64e624835cb982b58de3d001516540
Jordan Barnes <53000518-jordanmbarnes29@users.noreply.replit.com>
2026-02-02 18:37:24 +0000

    Add Capacitor Android Project

---
+++ android/app/src/main/java/com/ttjb/printcalculator/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ttjb.printcalculator;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ttjb.printcalculator',
+  appName: '3D Print Calculator',
+  webDir: 'dist'
+};
+
+export default config;
